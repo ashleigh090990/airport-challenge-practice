c65cf75fd9b31a668eb94d56524ad7f36a5e9343
Ashleigh Maund <ashleighmaund@gmail.com>
2015-05-05 00:48:35 +0100

    Cleaned up

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,16 +4,16 @@ class Airport
   attr_accessor :capacity, :planes
   
   def initialize
-  	@capacity = 6
-  	@planes = []
+    @capacity = 6
+    @planes = []
   end
 
   def permission_to_land plane
-  	fail 'Permission denied' if reached_capacity?
-  	@planes << plane
+    fail 'Permission denied' if reached_capacity?
+    @planes << plane
   end
 
   def reached_capacity?
-  	@planes.length >= capacity
+    @planes.length >= capacity
   end
 end
